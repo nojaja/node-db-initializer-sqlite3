ff91bfe344d121ce10000547ff5b712743582228
nojaja <free.riccia@gmail.com>
2022-01-14 20:15:31 +0900

    実行結果を表示するように修正

--- src/initdb.js
+++ src/initdb.js
@@ -3,59 +3,89 @@ import * as path from 'path'
 import initSqlJs from "sql.js/dist/sql-wasm.js";
 import { parse } from 'csv-parse/sync'
 import Handlebars from "handlebars";
-import { Console } from 'console';
 
 Handlebars.registerHelper('notEmpty', function (arg1, options) {
   return (arg1 && arg1 != "''") ? options.fn(this) : options.inverse(this);
 });
 
 export class Initdb {
-  constructor() {
+  constructor(debug) {
+    this.debug = debug || false
   }
 
-/**
- * SQLiteの読み込み
- * ex. const content = await initdb.init(configdata,options.input);
- * @param {jsonObject} settings
- * @param {string} dbfile_path filename or file descriptor
- * @returns {Promise} db Promise
- */
+  resultsprocessor(results) {
+    if(results){
+      for (const result of results) {
+        console.log("Result ")
+        const header = result.columns.reduce(function (previousValue, currentValue, index, array) {
+          return previousValue + "\t" + currentValue;
+        });
+        console.log(header)
+        for (const row of result.values) {
+          const rowtext = row.reduce(function (previousValue, currentValue, index, array) {
+            return previousValue + "\t" + currentValue;
+          });
+          console.log(rowtext)
+        }
+      }
+    }
+  }
+  /**
+   * SQLiteの読み込み
+   * ex. const content = await initdb.init(configdata,options.input);
+   * @param {jsonObject} settings
+   * @param {string} dbfile_path filename or file descriptor
+   * @returns {Promise} db Promise
+   */
   async init(settings, dbfile_path) {
+    const instance = this
     try {
       const _settings = settings || {}
-      if(!settings.workspace) settings.workspace = process.cwd()
+      if (!settings.workspace) settings.workspace = process.cwd()
       return await initSqlJs().then(function (SQL) {
         // Load the db  
         const db = (dbfile_path) ? new SQL.Database(new Uint8Array(fs.readFileSync(dbfile_path))) : new SQL.Database();
         for (const iterator of settings.data || []) {
-          console.log("Action ",iterator.action)
+          console.log("Action ", iterator.action)
           if (iterator.action === 'Execute-SQL' && iterator.sql) {
             try {
-              const res = db.exec(iterator.sql)
-              console.log("Result ",res)
-
+              const results = db.exec(iterator.sql)
+              instance.resultsprocessor(results)
             } catch (e) {
-              console.error(iterator, e.message);
+              if(instance.debug){
+                console.error(e.message, iterator);
+              }else{
+                console.error(e.message, iterator.sql);
+              }
             }
           }
 
           if (iterator.action === 'Execute-SQL' && iterator.file) {
             try {
-              const sqlPath = path.normalize(path.join(settings.workspace,iterator.file)).replace(/\\/g, "/")
+              const sqlPath = path.normalize(path.join(settings.workspace, iterator.file)).replace(/\\/g, "/")
               const sql = fs.readFileSync(sqlPath, "utf8");
               try {
-                const res = db.run(sql)
+                const results = db.exec(sql)
+                instance.resultsprocessor(results)
               } catch (e) {
-                console.error(e.message, sql);
+                if(instance.debug){
+                  console.error(e.message, sql, iterator);
+                }else{
+                  console.error(e.message, sql);
+                }
               }
             } catch (e) {
-              console.error(iterator, e.message);
+              if(instance.debug){
+                console.error(e.message, iterator);
+              }else{
+                console.error(e.message);
+              }
             }
           }
 
           if (iterator.action === 'Import-CSV' && iterator.file) {
             try {
-              const dataPath = path.normalize(path.join(settings.workspace,iterator.file)).replace(/\\/g, "/")
+              const dataPath = path.normalize(path.join(settings.workspace, iterator.file)).replace(/\\/g, "/")
               const data = fs.readFileSync(dataPath, "utf8");
 
               const preparesTemplate = Handlebars.compile(iterator.sql);
@@ -73,15 +103,27 @@ export class Initdb {
                   try {
                     const res = db.run(sql)
                   } catch (e) {
-                    console.error(e.message, sql, mod_values, e);
+                    if(instance.debug){
+                      console.error(e.message, sql, mod_values, e);
+                    }else{
+                      console.error(e.message, sql);
+                    }
                   }
                 } catch (e) {
-                  console.error(e.message, values, e);
+                  if(instance.debug){
+                    console.error(e.message, values, e);
+                  }else{
+                    console.error(e.message, values);
+                  }
                 }
               }
             }
             catch (e) {
-              console.error(iterator, e.message, e);
+              if(instance.debug){
+                console.error(iterator, e.message, e);
+              }else{
+                console.error(e.message);
+              }
             }
           }
         }
@@ -90,7 +132,11 @@ export class Initdb {
       })
 
     } catch (e) {
-      console.error(e.message, e);
+      if(instance.debug){
+        console.error(e.message, e);
+      }else{
+        console.error(e.message);
+      }
     }
   }
 
