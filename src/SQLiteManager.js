6220e2ea3cf586b2fa21c86d18792a206d18f190
nojaja <free.riccia@gmail.com>
2025-04-25 00:54:33 +0900

    wasmのパス修正 mainのパスを修正

--- src/SQLiteManager.js
+++ src/SQLiteManager.js
@@ -19,7 +19,8 @@ class SQLiteManager {
             const isPkg = process.pkg !== undefined;
             const wasmPath = isPkg
                 ? path.join(path.dirname(process.execPath), 'sql-wasm.wasm')
-                : path.join(process.cwd(), 'dist', 'sql-wasm.wasm');
+                : path.join(__dirname, 'sqlite3.wasm');
+                //path.join(process.cwd(), 'dist', 'sqlite3.wasm');
             const wasmBinary = fs.readFileSync(wasmPath);
             // Initialize wasm module
             sqlite3 = await init({
